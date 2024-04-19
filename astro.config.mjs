f15b0118a42ddc804d31da1b067701629e87f75e
Asp33ct <d.diaz.8861@gmail.com>
2024-04-19 13:40:45 -0700

    netupdate

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
+import robotsTxt from "astro-robots-txt";
+import UnoCSS from "@unocss/astro";
+import icon from "astro-icon";
+
+import solidJs from "@astrojs/solid-js";
+import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://osdaviddiazmaya.netlify.app",
+  integrations: [
+    sitemap(),
+    robotsTxt({
+      sitemap: [
+        "https://osdaviddiazmaya.netlify.app/sitemap-index.xml",
+        "https://osdaviddiazmaya.netlify.app/sitemap-0.xml",
+      ],
+    }),
+    solidJs(),
+    UnoCSS({ injectReset: true }),
+    icon()
+  ],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
+  output: "server",
+  adapter: netlify(),
+});
